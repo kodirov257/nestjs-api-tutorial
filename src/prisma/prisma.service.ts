ede5dfd2ce92c4f139985c16d5ed6a3fc7bcccca
Abdurakhmon Kodirov <akodirov@mercer.com>
2022-04-07 13:52:38 +0500

    Added PrismaModule and PrismaService

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: 'postgresql://root:123@localhost:11/nest?schema=public',
+        },
+      },
+    });
+  }
+}
