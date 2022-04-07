4fcf7f4618afe862df378841dae6f0d43b958261
Abdurakhmon Kodirov <akodirov@mercer.com>
2022-04-07 11:28:09 +0500

    Added some actions to AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 
-@Controller()
-export class AuthController {}
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  signup() {
+    return this.authService.signup();
+  }
+
+  @Post('signin')
+  signin() {
+    return this.authService.login();
+  }
+}
