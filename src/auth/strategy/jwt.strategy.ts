06a07fa14241df25e34deed1f8461a1716cc8341
Abdurakhmon Kodirov <akodirov@mercer.com>
2022-04-08 16:21:53 +0500

    Added JWT Strategy

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,16 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { Injectable } from '@nestjs/common';
+
+import { PrismaService } from '../../prisma/prisma.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(config: ConfigService, private prisma: PrismaService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get('JWT_SECRET'),
+    });
+  }
+}
