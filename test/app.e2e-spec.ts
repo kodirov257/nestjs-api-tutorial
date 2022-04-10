a1f7095197b9ca2a4947e859bab3cd5d0ee6ccc9
Abdurakhmon Kodirov <akodirov@mercer.com>
2022-04-11 01:10:56 +0500

    Created empty test functions

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,5 +1,6 @@
 import { Test } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
+import * as pactum from 'pactum';
 
 import { AppModule } from '../src/app.module';
 import { PrismaService } from '../src/prisma/prisma.service';
@@ -16,13 +17,48 @@ describe('App e2e', () => {
     app = moduleRef.createNestApplication();
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
+    await app.init();
+    await app.listen(3333);
+
     prisma = app.get(PrismaService);
     await prisma.cleanDb();
+
+    pactum.request.setBaseUrl('http://localhost:3333');
   });
 
   afterAll(() => {
     app.close();
   });
 
-  it.todo('should pass');
+  describe('Auth', () => {
+    describe('Signup', () => {
+      it.todo('should signup');
+    });
+
+    describe('Signin', () => {
+      it.todo('should signin');
+    });
+  });
+
+  describe('User', () => {
+    describe('Get me', () => {
+
+    });
+
+    describe('Edit user', () => {
+
+    });
+  });
+
+  describe('Bookmarks', () => {
+    describe('Create bookmark', () => {});
+
+    describe('Get bookmarks', () => {});
+
+    describe('Get bookmark by id', () => {});
+
+    describe('Edit bookmark by id', () => {});
+
+    describe('Delete bookmark by id', () => {});
+  });
 });
